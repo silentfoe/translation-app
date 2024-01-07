00fb80c44abf584068636591b9a94c3b0eab731d
Kevin Molid <kevinmolid@gmail.com>
2024-01-07 21:36:55 +0100

    Moved footer/header to separate HTML files

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Script is connected to HTML")
+// Load header
+fetch("header.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("header-placeholder").innerHTML = data
+    })
+
+// Load footer
+fetch("footer.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("footer-placeholder").innerHTML = data
+    })
