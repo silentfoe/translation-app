91ecf462dbae8b06be5697bfc660031b170d8dba
Kevin Molid <kevinmolid@gmail.com>
2024-01-08 21:39:18 +0100

    Added login form and signup/login toggle button

--- index.js
+++ index.js
@@ -11,3 +11,29 @@ fetch("footer.html")
     .then(data => {
         document.getElementById("footer-placeholder").innerHTML = data
     })
+
+
+// Add event listeners to document
+document.addEventListener("click", function(event){
+    // Toggle login form
+    if (event.target.id === 'login-toggle'){
+        // Toggle forms
+        document.getElementById('login-form').style.display = 'grid'
+        document.getElementById('signup-form').style.display = 'none'
+        // Toggle button colors
+        document.getElementById('login-toggle').classList.add('toggle-active')
+        document.getElementById('signup-toggle').classList.remove('toggle-active')
+    }
+
+    // Toggle login form
+    else if (event.target.id === 'signup-toggle'){
+        // Toggle forms
+        document.getElementById('signup-form').style.display = 'grid'
+        document.getElementById('login-form').style.display = 'none'
+        // Toggle button colors
+        document.getElementById('signup-toggle').classList.add('toggle-active')
+        document.getElementById('login-toggle').classList.remove('toggle-active')
+    }
+
+
+})
