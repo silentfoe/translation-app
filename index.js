c98675785c0022d01648dfab81bdc75ee8cb634a
mikegag <Michael.gagliardi1@hotmail.com>
2024-01-13 10:56:06 -0500

    Implemented 'Site language' page

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ fetch("header.html")
     .then(response => response.text())
     .then(data => {
         document.getElementById("header-placeholder").innerHTML = data
+        //must be loaded with initial header fetch to work
+        generateLanguageOptions()
     })
 
 // Load footer
@@ -34,6 +36,16 @@ document.addEventListener("click", function(event){
         document.getElementById('signup-toggle').classList.add('toggle-active')
         document.getElementById('login-toggle').classList.remove('toggle-active')
     }
-
-
 })
+
+//creates all language options for dropdown menu
+function generateLanguageOptions() {
+    //can be replaced later with an API as project scales 
+    const listOfLanguages = ["English", "Mandarin", "Hindi", "Spanish", "French", "Arabic", "Bengali", "Russian",
+    "Portuguese", "Urdu", "Indonesian", "German", "Japanese", "Nigerian", "Turkish", "Tamil"]
+
+    const formattedLanguageList = listOfLanguages.map(current=> {
+        return document.getElementById("language-dropdown-menu").innerHTML += 
+            `<button id="${current}-btn" class="dropdown-menu-option">${current}</button>`
+    })
+}
